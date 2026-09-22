2f0d1ee93d1f4ecda0953b2dd10cbec672917858
feng161110 <3515161453@qq.com>
2026-09-22 16:39:29 +0800

    第一次更新

--- src/export/json.ts
+++
@@ -1,5 +0,0 @@
-import type { MindMapIR } from "../types.js";
-
-export function irToJson(ir: MindMapIR): string {
-  return JSON.stringify(ir, null, 2);
-}
--- src/transform/rules/position.ts
+++
@@ -1,5 +0,0 @@
-export function lineOf(node: unknown): number | undefined {
-  const position = (node as { position?: { start?: { line?: number } } }).position;
-  const line = position?.start?.line;
-  return typeof line === "number" ? line - 1 : undefined;
-}
